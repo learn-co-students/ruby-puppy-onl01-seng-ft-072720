3a04fdce2916be8a95728ba08290e2763f6a759b
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-07 16:34:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,21 +7,21 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
   
   def save
-    self.class.all << self
-  end
+  @@all << self
+end
+  
 
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
+ def self.all
+    @@all
     end
-  end
+  
   
    def self.print_all
-    self.all.each{|dog| puts "dog.name"}
+    self.all.each{|dog| puts dog.name}
   end
 
   def self.clear_all
